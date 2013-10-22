11c7ab34ba64b36afbcb3f10b859a578d23573f7
cphelps76 <cphelps76@gmail.com>
2013-10-22 17:34:55 -0500

    add build scripts

---
+++ Gummy.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Bits and pieces borrowed from cvpcs and xoomdev build scripts
+# Required build variables,  adjust according to your own.
+# Path to toolchain
+  cco=~/kernel/arm-eabi-4.7/bin/arm-eabi-
+# Path to build your kernel
+  k=~/kernel/grouper
+# Directory for the any kernel updater
+  t=$k/tools/bbk
+
+# Date to add to zip
+  today=$(date +"%m_%d_%Y")
+
+# Clean old builds
+   echo "Clean"
+     rm -rf $k/out
+     rm -rf $k/arch/arm/boot/zImage
+     make clean
+
+# Setup the build
+ cd $k/arch/arm/configs/BBKconfigs
+    for c in *
+      do
+        cd $k
+# Setup output directory
+       mkdir -p "out/$c"
+          cp -R "$t/system" out/$c
+          cp -R "$t/META-INF" out/$c
+          cp -R "$t/kernel" out/$c
+       mkdir -p "out/$c/system/lib/modules/"
+
+  m=$k/out/$c/system/lib/modules
+  z=$c-$today
+
+# Compile Kernels
+   echo ""
+   echo "Compiling $c"
+   echo ""
+
+   export ARCH=arm CROSS_COMPILE=$cco
+   cp $k/arch/arm/configs/BBKconfigs/$c "$k/.config"
+   make -j8 zImage
+
+# Grab modules & zImage
+   echo ""
+   echo "<<>><<>>  Collecting modules and zimage <<>><<>>"
+   echo ""
+   cp $k/arch/arm/boot/zImage out/$c/kernel/zImage
+   for mo in $(find . -name "*.ko"); do
+		cp "${mo}" $m
+   done
+
+# Build Zip
+ clear
+   x=Gummy-Kernel_Grouper-$today
+   echo "Creating $z.zip"
+     cd $k/out/$c/
+       7z a "$x.zip"
+         mv $x.zip $k/out/$x.zip
+           rm -rf $k/out/$c
+# Line below for debugging purposes,  uncomment to stop script after each config is run
+#read this
+      done
